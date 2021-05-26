a5ced63cd14f909216c2fc04c445fcb786109a7a
Dhyego Martins <dfam92@hotmail.com>
2021-05-26 17:43:23 -0300

    PlayerMovement was Added
    Obstacles added
    player movement added

---
+++ PuzzleGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    [SerializeField] private float speed;
+    public GameObject cameraPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void PlayerMovement()
+    {
+        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(cameraPlayer.transform.up * vInput * speed);
+        playerRb.AddForce(cameraPlayer.transform.right * hInput * speed);
+       
+    }
+}
